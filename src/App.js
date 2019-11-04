b1006223321a98ad1e1b3c3c4507470894f005c6
David Tauraso <david318@me.com>
2019-11-04 14:41:53 -0800

    Added a button to change the quarter

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import "./App.css";
 import BottomRow from "./BottomRow";
 import ScoreBoard from "./ScoreBoard.js";
 import TeamButtons from "./TeamButtons.js";
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   let [homeScore, setHomeScore] = useState(0)
   let [otherScore, setOtherScore] = useState(0)
+  let [quarterValue, setQuarterValue] = useState(0)
+  const setScores = (teamName, pointsAwarded) => {
 
+      if(teamName === "home") {
+        setHomeScore(homeScore + pointsAwarded)
+      } else if(teamName === "other") {
+        setOtherScore(otherScore + pointsAwarded)
+      }
+  }
+
+  
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
 
-          <ScoreBoard teamClass="home" teamNameClass="home__name" teamName="Lions" teamScoreClass="home__score" teamScore={homeScore} />
+          <ScoreBoard teamClass="home"
+                      teamNameClass="home__name"
+                      teamName="Lions"
+                      teamScoreClass="home__score"
+                      teamScore={homeScore} />
           {/* <div className="home"> */}
 
             {/* <h2 className="home__name">Lions</h2> */}
@@ -27,26 +42,33 @@ function App() {
 
           
           <div className="timer">00:03</div>
-          <ScoreBoard teamClass="away" teamNameClass="away__name" teamName="Tigers" teamScoreClass="away__score" teamScore={otherScore} />
-          {/* <div className="away"> */}
-            {/* <h2 className="away__name">Tigers</h2> */}
-            {/* <div className="away__score">{otherScore}</div> */}
-          {/* </div> */}
+          <ScoreBoard teamClass="away"
+                      teamNameClass="away__name"
+                      teamName="Tigers"
+                      teamScoreClass="away__score"
+                      teamScore={otherScore} />
         </div>
-        <BottomRow />
+        <BottomRow myValue={quarterValue}/>
       </section>
       <section className="buttons">
-        <TeamButtons teamNameButtons="homeButtons" teamTouchdownClass="homeButtons__touchdown" teamFieldGoalClass="homeButtons__fieldGoal" setTeamTouchdownScore={() => {setHomeScore(homeScore + 7)}} setTeamFieldGoalScore={() => {setHomeScore(homeScore + 3)}} teamTouchdown="Home Touchdown" teamFieldGoal="Home Field Goal" />
-        {/* <div className="homeButtons"> */}
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          {/* <button className="homeButtons__touchdown" onClick={() => {setHomeScore(homeScore + 7)}}>Home Touchdown</button> */}
-          {/* <button className="homeButtons__fieldGoal" onClick={() => {setHomeScore(homeScore + 3)}}>Home Field Goal</button> */}
-        {/* </div> */}
+        <TeamButtons  teamNameButtons="homeButtons"
+                      teamTouchdownClass="homeButtons__touchdown"
+                      teamFieldGoalClass="homeButtons__fieldGoal"
+                      setTeamTouchdownScore={() => {setScores("home", 7)}}
+                      setTeamFieldGoalScore={() => {setScores("home", 3)}}
+                      teamTouchdown="Home Touchdown"
+                      teamFieldGoal="Home Field Goal" />
 
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => {setOtherScore(otherScore + 7)}}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => {setOtherScore(otherScore + 3)}}>Away Field Goal</button>
-        </div>
+        <TeamButtons  teamNameButtons="awayButtons"
+                      teamTouchdownClass="awayButtons__touchdown"
+                      teamFieldGoalClass="awayButtons__fieldGoal"
+                      setTeamTouchdownScore={() => {setScores("other", 7)}}
+                      setTeamFieldGoalScore={() => {setScores("other", 3)}}
+                      teamTouchdown="Away Touchdown"
+                      teamFieldGoal="Away Field Goal" />
+        {/* mod whatever the value is then add 1 to prevent it from being 0 */}
+        <button className="homeButtons__touchdown" onClick={() => {setQuarterValue( (quarterValue % 4) + 1 )}}>Increase Quarter</button>
+
       </section>
     </div>
   );
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -2,31 +2,44 @@ import React from "react";
 import "./App.css";
 import GameStats from "./GameStats.js";
 
-const BottomRow = () => {
+const BottomRow = (props) => {
+
+  // let [quarter, setQuarter] = useState(4);
+
+  // const updateQuarter = (myQuarter) => {
+  //   setQuarter((myQuarter + 1) % 4)
+  // }
+  
+  console.log(props.myValue)
+  // updateQuarter(props.myValue);
   return (
     <div className="bottomRow">
 
-      <GameStats statsKind="down" statsTitleClass="down__title" statsValueClass="down__value" statsTitle="Down" statsValue={3} />
-      {/* <div className="down"> */}
-        {/* <h3 className="down__title">Down</h3> */}
-        {/* <div className="down__value">3</div> */}
-      {/* </div> */}
+      <GameStats  statsKind="down"
+                  statsTitleClass="down__title"
+                  statsValueClass="down__value"
+                  statsTitle="Down"
+                  statsValue={3} />
 
-      <GameStats statsKind="toGo" statsTitleClass="toGo__title" statsValueClass="toGo__value" statsTitle="To Go" statsValue={7} />
-      {/* <div className="toGo"> */}
-        {/* <h3 className="toGo__title">To Go</h3> */}
-        {/* <div className="toGo__value">7</div> */}
-      {/* </div> */}
-      <GameStats statsKind="ballOn" statsTitleClass="ballOn__title" statsValueClass="ballOn__value" statsTitle="Ball on" statsValue={21} />
-      {/* <div className="ballOn"> */}
-        {/* <h3 className="ballOn__title">Ball on</h3> */}
-        {/* <div className="ballOn__value">21</div> */}
-      {/* </div> */}
-      <GameStats statsKind="quarter" statsTitleClass="quarter__title" statsValueClass="quarter__value" statsTitle="Quarter" statsValue={4} />
-      {/* <div className="quarter"> */}
-        {/* <h3 className="quarter__title">Quarter</h3> */}
-        {/* <div className="quarter__value">4</div> */}
-      {/* </div> */}
+      <GameStats  statsKind="toGo"
+                  statsTitleClass="toGo__title"
+                  statsValueClass="toGo__value"
+                  statsTitle="To Go"
+                  statsValue={7} />
+
+      <GameStats  statsKind="ballOn"
+                  statsTitleClass="ballOn__title"
+                  statsValueClass="ballOn__value"
+                  statsTitle="Ball on"
+                  statsValue={21} />
+
+      <GameStats  statsKind="quarter"
+                  statsTitleClass="quarter__title"
+                  statsValueClass="quarter__value"
+                  statsTitle="Quarter"
+                  statsValue={props.myValue} />
+      {/* <button className="homeButtons__touchdown" onClick={() => {}}>testing</button> */}
+
     </div>
   );
 };
